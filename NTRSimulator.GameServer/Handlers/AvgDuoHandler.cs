a4eac3ffcfc0e0584c42e02c0803fb78d35f7e68
raphaeIl <michaelsliu3@gmail.com>
2026-06-22 19:37:15 -0400

    Unlock all main and substages for All Lobby special cg

--- NTRSimulator.GameServer/Handlers/AvgDuoHandler.cs
+++ NTRSimulator.GameServer/Handlers/AvgDuoHandler.cs
@@ -1,17 +1,35 @@
 using NTRSimulator.Common.Networking;
 using NTRSimulator.Common.Proto;
+using NTRSimulator.Common.Table;
 
 namespace NTRSimulator.GameServer.Handlers
 {
-    public sealed class AvgDuoHandler : AvgDuoHandlerBase
+    public sealed class AvgDuoHandler(ITableService tableService) : AvgDuoHandlerBase
     {
         public override void HandleGetLobbbyAvgDuoInfo(CS_GetLobbbyAvgDuoInfo request, Connection connection)
         {
-            connection.Send(new SC_GetLobbbyAvgDuoInfo
+            List<Avg3dDuoStageData> avg3dDuoStageData = tableService.GetTable<Avg3dDuoStageData>();
+
+            SC_GetLobbbyAvgDuoInfo lobbyAvgDuoInfo = new SC_GetLobbbyAvgDuoInfo
             {
+                // main plot
                 Field1 = { },
-                Field2 = { },
-            });
+
+                // sub plot
+                Field2 = { }
+            };
+
+            foreach (Avg3dDuoStageData avg3dDuoStage in avg3dDuoStageData)
+            {
+                lobbyAvgDuoInfo.Field1[avg3dDuoStage.Field1] = true;
+
+                foreach (uint avg3dDuoSubStage in avg3dDuoStage.Field2)
+                {
+                    lobbyAvgDuoInfo.Field2[avg3dDuoSubStage] = true;
+                }
+            }
+
+            connection.SendAutoEncrypted(lobbyAvgDuoInfo);
         }
     }
 }
